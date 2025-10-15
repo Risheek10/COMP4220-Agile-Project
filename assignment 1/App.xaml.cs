1bd4977c52bf9c358d53d138a8d81e86645460cd
Ali Almaamouri <alialm05@proton.me>
2025-10-15 12:44:29 -0400

    base project

---
+++ assignment 1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ywBookStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Ensure DataDirectory is set before any DB code runs.
+            var exeFolder = AppDomain.CurrentDomain.BaseDirectory; // bin\Debug\...
+            var dataFolder = Path.Combine(exeFolder, "Data");
+            Directory.CreateDirectory(dataFolder); // safe if it exists
+
+            AppDomain.CurrentDomain.SetData("DataDirectory", dataFolder);
+        }
+    }
+}
