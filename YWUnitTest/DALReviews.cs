c0001978c3638db629b0a0030092698b3a83e44b
tnxie <tina.yihan.xie@gmail.com>
2025-11-24 17:22:18 -0800

    review and rating (write & view)

---
+++ YWUnitTest/DALReviews.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ywBookStoreLIB
+{
+    public class DALReviews
+    {
+        private string connStr = Properties.Settings.Default.ywConnectionString;
+
+        public DataTable GetReviewsByISBN(string isbn)
+        {
+            string query = @"SELECT UserID, Rating, ReviewText, CreatedAt
+                             FROM Reviews
+                             WHERE ISBN = @isbn
+                             ORDER BY CreatedAt DESC";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@isbn", isbn);
+
+                DataTable dt = new DataTable();
+                conn.Open();
+                new SqlDataAdapter(cmd).Fill(dt);
+                return dt;
+            }
+        }
+
+        public void AddReview(string isbn, int userId, int rating, string review)
+        {
+            string query = @"INSERT INTO Reviews (ISBN, UserID, Rating, ReviewText)
+                             VALUES (@isbn, @userId, @rating, @review)";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@isbn", isbn);
+                cmd.Parameters.AddWithValue("@userId", userId);
+                cmd.Parameters.AddWithValue("@rating", rating);
+                cmd.Parameters.AddWithValue("@review", review);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ YWUnitTest/DatabaseHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ywBookStoreLIB
+{
+    public class DatabaseHelper
+    {
+        private readonly string connectionString;
+
+        public DatabaseHelper()
+        {
+            // 
+        }
+
+        public void ExecuteNonQuery(string sql)
+        {
+            DataTable dt = new DataTable();
+            string connectionString = Properties.Settings.Default.ywConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataTable ExecuteQuery(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+            {
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
