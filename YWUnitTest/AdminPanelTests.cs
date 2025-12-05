3c28d8511b8166fea9a51d92729c6179adf77c59
Ali <ali@kalakech.com>
2025-12-05 02:20:34 -0500

    Refactor: Exclude database-dependent tests from CI workflow.

--- YWUnitTest/AdminPanelTests.cs
+++ YWUnitTest/AdminPanelTests.cs
@@ -6,6 +6,7 @@ using System;
 namespace YWUnitTest
 {
     [TestClass]
+    [TestCategory("Database")]
     public class AdminPanelTests
     {
         private static readonly object dbLock = new object();
