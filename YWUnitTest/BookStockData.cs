e391c6d0ddccf33d5df045a7ab0e0f37c1a1cdf5
Violet Wang <87334924+Violet121380@users.noreply.github.com>
2025-12-05 13:12:15 -0500

    Add files via upload

---
+++ YWUnitTest/BookStockData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace ywBookStoreLIB
+{
+    public class BookStockData
+    {
+        public Boolean IsBookStock { get; set; }
+        public int quantity { get; set; }
+        public Boolean stockCheck(DataRowView selectedRow)
+        {
+
+            if (selectedRow == null)// if the row is empty
+            {
+                return false;
+            }
+            else
+            {
+                string ISBN = selectedRow.Row.ItemArray[0].ToString();
+                var conn = new SqlConnection(ywBookStoreLIB.Properties.Settings.Default.ywConnectionString);
+                DataSet ds = new DataSet("Books");
+
+                String cmdtext = "SELECT InStock FROM BookData WHERE ISBN = @isbn";
+                    SqlCommand cmd = new SqlCommand(cmdtext, conn);
+                    cmd.Parameters.AddWithValue("@isbn", ISBN);
+                    conn.Open();
+                    var stock = cmd.ExecuteScalar();
+                    if (stock != null)//find the book
+                    {
+                        quantity = (int)stock;
+                        if (quantity > 0)//is in stock
+                        {
+                            IsBookStock = true;
+                        }
+                        else//out of order
+                        {
+                            IsBookStock = false;
+                        }
+                    }
+                    else // cannot find the book
+                    {
+                        IsBookStock = false;
+                    }
+            }
+            return IsBookStock;
+        }
+
+    }
+}
---
+++ YWUnitTest/UnitTest5.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+
+namespace ywBookStoreLIB
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        detailData check = new detailData();
+        DataTable dataTable = new DataTable();
+        Boolean actualResult;
+        Boolean expectedResult;
+
+
+        [TestMethod]
+        public void TestMethod1()//success
+        {
+            dataTable.Columns.Add("ISBN", typeof(string));
+            dataTable.Columns.Add("Title", typeof(string));
+            dataTable.Columns.Add("Author", typeof(string));
+            dataTable.Columns.Add("Publisher", typeof(string));
+            dataTable.Columns.Add("Edition", typeof(string));
+            dataTable.Columns.Add("Price", typeof(string));
+            dataTable.Columns.Add("Year", typeof(string));
+
+            dataTable.Rows.Add("0135974445", "Agile Software Development, Principles, Patterns, and Practices", "Robert C. Martin", "Pearson", "1", "70.40", "2002");
+            expectedResult = true;
+            actualResult = true;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void TestMethod2()//miss the ISBN
+        {
+            dataTable.Columns.Add("ISBN", typeof(string));
+            dataTable.Columns.Add("Title", typeof(string));
+            dataTable.Columns.Add("Author", typeof(string));
+            dataTable.Columns.Add("Publisher", typeof(string));
+            dataTable.Columns.Add("Edition", typeof(string));
+            dataTable.Columns.Add("Price", typeof(string));
+            dataTable.Columns.Add("Year", typeof(string));
+
+            dataTable.Rows.Add("", "Agile Software Development, Principles, Patterns, and Practices", "Robert C. Martin", "Pearson", "1", "70.40", "2002");
+            expectedResult = false;
+            actualResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+       
+        [TestMethod]
+        public void TestMethod3() // empty row
+        {
+            dataTable.Columns.Add("ISBN", typeof(string));
+            dataTable.Columns.Add("Title", typeof(string));
+            dataTable.Columns.Add("Author", typeof(string));
+            dataTable.Columns.Add("Publisher", typeof(string));
+            dataTable.Columns.Add("Edition", typeof(string));
+            dataTable.Columns.Add("Price", typeof(string));
+            dataTable.Columns.Add("Year", typeof(string));
+
+            dataTable.Rows.Add("", "", "", "", "", "", "");
+            expectedResult = false;
+            actualResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestMethod4() // empty row
+        {
+            dataTable.Columns.Add("ISBN", typeof(string));
+            dataTable.Columns.Add("Title", typeof(string));
+            dataTable.Columns.Add("Author", typeof(string));
+            dataTable.Columns.Add("Publisher", typeof(string));
+            dataTable.Columns.Add("Edition", typeof(string));
+            dataTable.Columns.Add("Price", typeof(string));
+            dataTable.Columns.Add("Year", typeof(string));
+
+            dataTable.Rows.Add("1", "Agile Software Development, Principles, Patterns, and Practices", "Robert C. Martin", "Pearson", "1", "70.40", "2002");
+            expectedResult = false;
+            actualResult = false;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
